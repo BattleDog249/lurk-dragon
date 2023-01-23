acdbfb802d8a97e5d0fa76437c0b1a28d1f518a7
Logan Gray <lgray@proton.me>
2023-01-23 12:37:49 -0800

    More basic stuff

--- client-test.py
+++ client-test.py
@@ -5,11 +5,15 @@
 
 import socket   # Import necessary module
 
-skt = socket.socket() # Establish socket
+skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Establish socket
+print('DEBUG: skt = ', skt)
 
 # Assign host IP/name and port number
 host = 'localhost' # Should use socket.gethostname()
 port = 5125
+
+print('DEBUG: host = ', host)
+print('DEBUG: port = ', port)
 
 skt.connect((host, port))   # Connect to server
 
@@ -17,6 +21,7 @@ skt.connect((host, port))   # Connect to server
 # Since the message is sent in bytes, we have to decode it before printing
 print('DEBUG: Recieved server message: ', skt.recv(1024).decode())
 
-#skt.send(bytes('Happy to connect with you!','utf-8')) #sending the TCP message to the server in bytes form
+# Send TCP message to server in bytes form
+#skt.send(bytes('Happy to connect with you!','utf-8'))
 
 skt.close() # Close connection to server
--- server-test.py
+++ server-test.py
@@ -6,29 +6,31 @@
 import socket
 
 skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create socket named skt
-print('DEBUG: skt = ', skt)
+print('DEBUG: skt  = ', skt)
 
+# Assign host IP/name and port number
 host = 'localhost'  # Should use socket.gethostname()
 port = 5125         # Testing port number
 maxConnections = 1  # Assign number of connections server can handle
 
 print('DEBUG: host = ', host)
 print('DEBUG: port = ', port)
+print('DEBUG: max  = ', maxConnections)
 
 skt.bind((host, port))  # Bind to IP address of server and provided port number
 
 skt.listen(maxConnections) # Listens and waits for 1 client
 
-print('Waiting for connection...')
+print('DEBUG: Listening...')
 
 # Loop for each client that connects
 while True:
     # Accepts connection from client & returns client name and address
-    (client, address) = skt.accept()
+    client_fd, addr = skt.accept()
 
-    print('DEBUG:   Client connecting from: ', address)
+    print('DEBUG:   Client connecting from: ', client_fd)
     
-    client.send(bytes('\nConnected to server-test.py', 'utf-8')) #sending a TCP message to the client in the form of bytes 
+    client_fd.send(bytes('\nConnected to server-test.py', 'utf-8')) #sending a TCP message to the client in the form of bytes 
 
     #client_msg = skt.recv(1024).decode() #getting the TCP message from the client and decoding it 
     
