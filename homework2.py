daa7777541bba02328d847aaa9683bb1ec327810
Logan Gray <lgray@proton.me>
2023-01-30 11:17:43 -0800

    Testing

--- homework2.py
+++ homework2.py
@@ -1,7 +1,7 @@
 '''
     Due Wednesday, February 1
     Connect to port 5071 on isoptera.
-    There is a server running on that port that will send you 5 32-bit integers in big endian order.
+    There is a server running on that port that will send you 5 32-bit (20 bytes total) integers in big endian order.
     Upload a text file with your name, the integers isoptera sent you, and explain how you received them (include source code if you wrote it).
     You are welcome to start with the simple_client.c program from class, but you can just start your own program if you prefer. 
 '''
@@ -9,15 +9,17 @@
 import socket
 import struct
 
+address = 'isoptera.lcsc.edu'
+port = 5071
+
 skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect to Isoptera directly, no DNS lookups
-skt.connect(('74.118.22.194', 5071))
+# Connect to Isoptera
+skt.connect((socket.gethostbyname(address), port))
 
-message = skt.recv(1024)
-
+# Recieve 20 bytes, store in 32 byte buffer
+message = skt.recv(32)
 print('Raw Message: ', message)
 
 value = int.from_bytes(message, 'big', signed = False)
-
 print(value)
