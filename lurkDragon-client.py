0aeee279f76a4e86692ba4d8699fb4188b2a17e1
Logan Gray <lgray@proton.me>
2023-02-02 12:45:53 -0800

    Add send/recvCharacter() function

--- lurkDragon-client.py
+++ lurkDragon-client.py
@@ -20,7 +20,19 @@ def recvGame():
     game_msg = struct.unpack("<B3H", skt.recv(7))
     game_des = skt.recv(game_msg[3])                    # Read game description, recv only description length
     print('DEBUG: Received GAME message:', game_msg)
-    print('DEBUG: Received GAME message:', game_des)
+    print('DEBUG: Received GAME message:', game_des.decode())
+    return 0
+
+def sendCharacter(t = 10, name = "Test Dummy", flags = 0, attack = 50, defense = 25, regen = 5, health = 20, gold = 0, room = 0):
+    """
+    Function to send CHARACTER message to server
+    NEEDS WORK
+    """
+    characterDescription = "This is a test dummy, don't feel bad for it."
+    characterBytes = struct.pack('<B%dcB7H' %len(name), t, name, flags, attack, defense, regen, health, gold, room, len(characterDescription))
+    desBytes = bytes(characterDescription, 'utf-8')
+    skt.sendall(characterBytes)
+    skt.sendall(desBytes)
     return 0
 
 # Establish IPv4 TCP socket
@@ -28,14 +40,19 @@ skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Variables defining host address & port of server to connect to
 host = 'localhost'
-port = 5126
+port = 5195
 
 # Connect to server with assigned host & port
 skt.connect((socket.gethostname(), port))
 print('DEBUG: Connecting to server:', host)
 
-recvVersion()
-recvGame()
+version = recvVersion()
+game = recvGame()
 
-skt.close() # Close connection to server
-print('DEBUG: Closed connection')
+if game == 0:
+    sendCharacter()
+    print('DEBUG: Sent CHARACTER message')
+
+#skt.shutdown(2) # Not necessary AFAIK
+#skt.close() # Close connection to server
+#print('DEBUG: Closed connection')
--- lurkDragon-server.py
+++ lurkDragon-server.py
@@ -6,15 +6,17 @@ CS435 LurkDragon: Server
 
 # Import socket module, necessary for network communications
 import socket
+# Import struct module, required for packing/unpacking structures
 import struct
+import threading
 
 gameDescription = "This is Logan's testing server description! Not very exciting, yet......"
 
 # Function for sending VERSION message to client
 def sendVersion(t = 14, major = 2, minor = 3, extSize = 0):
-    '''
+    """
     Sent by the server upon initial connection along with GAME.
-    '''
+    """
     versionBytes = struct.pack("<3BH", t, major, minor, extSize)
     #print(versionBytes)
     #versionMsg = struct.unpack("<bbbh", versionBytes)
@@ -25,13 +27,13 @@ def sendVersion(t = 14, major = 2, minor = 3, extSize = 0):
 
 # Function for sending GAME message to client
 def sendGame(t = 11, initPoints = 100, statLimit = 65535):
-    '''
+    """
     Used by the server to describe the game. The initial points is a combination of health, defense, and regen, and cannot be exceeded by the client when defining a new character.
     The stat limit is a hard limit for the combination for any player on the server regardless of experience.
     If unused, it should be set to 65535, the limit of the unsigned 16-bit integer.
     This message will be sent upon connecting to the server, and not re-sent.
-    '''
-    gameBytes = struct.pack("<B3H", t, initPoints, statLimit, len(gameDescription)) #gameDescription)  # Not yet sure what format to use for gameDescription
+    """
+    gameBytes = struct.pack("<B3H", t, initPoints, statLimit, len(gameDescription))
     desBytes = bytes(gameDescription, 'utf-8')
     #print(gameBytes)
     #print(desBytes)
@@ -42,11 +44,31 @@ def sendGame(t = 11, initPoints = 100, statLimit = 65535):
     print('DEBUG: GAME sent!')
     return 0
 
+def recvCharacter():
+    """
+    NEEDS WORK
+    """
+    characterMsg = struct.unpack('<B32cB7H', clientSkt.recv(48))
+    characterDes = clientSkt.recv(characterMsg[10])
+    print('DEBUG: Received CHARACTER message:', characterMsg)
+    print('DEBUG: Received CHARACTER Description:', characterDes)
+    return 0
+    
+
+def initConnect():
+    """
+    Executed when a client connects to the server, sends VERSION & GAME message to client
+    """
+    sendVersion()
+    sendGame()
+    recvCharacter()
+    return 0
+
 # Establish IPv4 TCP socket
 serverSkt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Assign port number
-port = 5126
+port = 5195
 
 # Bind server to machine's hostname & assigned port number
 serverSkt.bind((socket.gethostname(), port))
@@ -57,11 +79,14 @@ print('Waiting for connection...')
 
 while True:
     # Accepts connection from client & returns client socket (file descriptor) and address
-    clientSkt, addr = serverSkt.accept()            # Accept & assign client connection to clientSkt
+    clientSkt, addr = serverSkt.accept()                    # Accept & assign client connection to clientSkt
     print('DEBUG: Client Socket:', clientSkt)
     print('DEBUG: Client Address:', addr)
-    sendVersion()                                   # Send VERSION to client
-    sendGame()                                      # Send GAME to client
+    clientThread = threading.Thread(target = initConnect)   # Create thread for connected client
+    clientThread.start()                                    # Start thread
+    print("DEBUG: Client Thread:", clientThread)
+    #sendVersion()                                          # Send VERSION to client
+    #sendGame()                                             # Send GAME to client
 
 #client_msg = skt.recv(1024).decode() # Get message from client and decode
 #print('DEBUG: Client {client_fd} message is {client_msg}')
