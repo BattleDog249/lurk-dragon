c08cf67412ee529d0b28557ae35aa6a2f7e73957
Logan Gray <lhgray@lcmail.lcs.edu>
2023-02-25 23:16:21 -0800

    Bug fixes

--- serverlib.py
+++ serverlib.py
@@ -11,8 +11,8 @@ class Client:
         Client.clients[skt] = skt.fileno()              # Add file descriptor to dictionary for tracking connections
         print('DEBUG: Added Client: ', Client.clients[skt])
     def removeClient(skt):
+        print('DEBUG: Removing Client: ', Client.clients[skt])
         Client.clients.pop(skt)
-        print('DEBUG: Removed Client: ', Client.clients[skt])
         #print('DEBUG: Connected Clients:', Client.clients)
     def getClients():                   # Pull list of all connected clients
         return Client.clients
