f770d24dcee1899ccc9f790222274bb29550d75f
Logan Gray <lhgray@lcmail.lcs.edu>
2023-03-03 10:34:48 -0800

    START work

--- lurklib.py
+++ lurklib.py
@@ -153,7 +153,9 @@ class Lurk:
                     print('ERROR: Failed to unpack START data!')
                     i += startHeaderLen
                     continue
+                print('DEBUG: Appending START:', msgType)
                 messages.append((msgType,))
+                print('DEBUG: List of messages before continuing:', messages)
                 i += startHeaderLen
                 continue
             
