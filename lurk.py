e0d6c507061aac92bdd480522dbab75f8206d199
Logan Gray <lgray@proton.me>
2023-03-07 12:56:37 -0800

    Cleanup

--- lurk.py
+++ lurk.py
@@ -230,9 +230,7 @@ def read(skt):
                     return None
                 print(f'DEBUG: read: Potential CHARACTER data: {lurk_data}')
                 char_des, = struct.unpack(f'<{char_des_len}s', lurk_data)
-                name = name.decode('utf-8', 'ignore')
-                char_des = char_des.decode('utf-8', 'ignore')
-                return (CHARACTER, name, flags, attack, defense, regen, health, gold, room, char_des_len, char_des)     # Issue here with lurktest -t; cannot decode 0xff with utf-8?
+                return (CHARACTER, name.decode('utf-16'), flags, attack, defense, regen, health, gold, room, char_des_len, char_des.decode('utf-16'))     # Issue here with lurktest -t; cannot decode 0xff with utf-8?
             except struct.error:
                 print(Fore.RED+'ERROR: read: Failed to unpack CHARACTER!')
                 continue
