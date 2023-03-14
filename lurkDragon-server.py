4cca5bd475c10772ffacd3907cb67a68c64be269
Logan Gray <lgray@proton.me>
2023-03-14 12:30:40 -0700

    Debugging

--- lurkDragon-server.py
+++ lurkDragon-server.py
@@ -75,9 +75,11 @@ def get_character(name):
     Returns:
         _type_: _description_
     """
-    if name not in characters.items():
-        print(Fore.RED+f'ERROR: get_character: Cannot find {name} in {characters.items()}!')
+    '''
+    if name not in characters.keys():
+        print(Fore.RED+f'ERROR: get_character: Cannot find {name} in {characters.keys()}!')
         return None
+    '''
     character = (name, characters[name][0], characters[name][1], characters[name][2], characters[name][3], characters[name][4], characters[name][5], characters[name][6], characters[name][7], characters[name][8])
     return character
 def send_characters(room_num):
