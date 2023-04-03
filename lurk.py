072ea40375fa74d7017db9e198697837af323e9b
Logan Gray <lhgray@lcmail.lcs.edu>
2023-04-03 07:11:07 -0700

    Testing

--- lurk.py
+++ lurk.py
@@ -63,7 +63,7 @@ class Character:
     def get_character_with_name(name):
         """ Returns a character with the given name. If the character is not found, returns None.
         """
-        character = [(name, stat) for name, stat in Character.characters.items() if name in Character.characters]
+        character = [(name, stat) for name, stat in Character.characters.items() if name in Character.characters and name == name]
         print(f'DEBUG: Character found with name {name}: {character}')
         '''
         if name in Character.characters:
