8f72da57931e5a6ba392b58b046c01d5ebbd7ca0
Logan Gray <lhgray@lcmail.lcs.edu>
2023-04-03 07:15:46 -0700

    Testing

--- lurk.py
+++ lurk.py
@@ -64,6 +64,7 @@ class Character:
         """ Returns a character with the given name. If the character is not found, returns None.
         """
         character = [(name, stat) for name, stat in Character.characters.items() if name in Character.characters and target_name == name]
+        character = Character(name=character[0][0], flag=character[0][1][0], attack=character[0][1][1], defense=character[0][1][2], regen=character[0][1][3], health=character[0][1][4], gold=character[0][1][5], room=character[0][1][6], description_len=character[0][1][7], description=character[0][1][8])
         print(f'DEBUG: Character found with name {target_name}: {character}')
         '''
         if name in Character.characters:
