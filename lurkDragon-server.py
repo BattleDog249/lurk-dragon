4878d7f0006958b06c76d254f91852158916d71a
Logan Gray <lhgray@lcmail.lcs.edu>
2023-04-03 13:43:42 -0700

    Testing

--- lurkDragon-server.py
+++ lurkDragon-server.py
@@ -272,7 +272,7 @@ def handle_client(skt):
             count = 0
             characters = lurk.Character.get_characters_with_room(player.room)
             for character in characters:
-                if character.flag != (lurk.ALIVE & lurk.MONSTER):
+                if character.flag != (lurk.ALIVE | lurk.MONSTER):
                     continue
                 print(Fore.WHITE+f'DEBUG: {character.name} has monster flag set, flag: {character.flag}')
                 count+=1
