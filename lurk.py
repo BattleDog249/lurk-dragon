89910c13efd8aae131834176b4e368afc6819d33
Logan Gray <lgray@proton.me>
2023-04-06 13:55:41 -0700

    Debugging

--- lurk.py
+++ lurk.py
@@ -219,7 +219,7 @@ class Room:
     def get_room(number):
         """Returns a room with the given number. If the room is not found, returns None."""
         room = [(room_number, room_info) for room_number, room_info in Room.rooms.items() if number in Room.rooms and number == room_number]
-        room = Room(number=room[0][0], name=room[0][1][0], description_len=room[0][1][1], description=room[0][1][2])
+        room = Room(number=room[0][0], name=room[0][1][0], description_len=room[0][1][1], description=room[0][1][2], connections=room[0][1][3])
         return room
     def recv_room(skt):
         """Receives a room message from the given socket, and unpacks it into a room object that is returned, or None if an error occurred."""
