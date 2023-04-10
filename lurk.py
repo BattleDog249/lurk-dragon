66c97d24ed2651d89f7d6813f01f6823ea4499b5
Logan <lhgray@lcmail.lcsc.edu>
2023-04-10 12:06:05 -0700

    Testing

--- lurk.py
+++ lurk.py
@@ -328,9 +328,8 @@ class Character:
     characters = {}
     def get_character_with_name(target_name):
         """Returns a character with the given name. If the character is not found, returns None."""
-        for character in Character.characters:
-            if Character.characters[character].name == target_name:
-                character_with_name = Character.characters[character]
+        character_with_name = [character for character in Character.characters if Character.characters[character].name == target_name]
+            #character_with_name = Character.characters[character]
         return character_with_name
     def get_characters_with_room(room):
         """Returns a list of character objects that are in the given room. If no characters are found, returns an empty list."""
