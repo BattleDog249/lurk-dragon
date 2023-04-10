ec7d7d5dcd787ef10788c15f57aa1ed27712dd82
Logan <lhgray@lcmail.lcsc.edu>
2023-04-10 09:55:44 -0700

    Bug fixes

--- lurkDragon-server.py
+++ lurkDragon-server.py
@@ -124,26 +124,20 @@ def handle_client(skt):
                 print(f"{Fore.YELLOW}WARN: Player not ready, sending ERROR code 5!")
                 lurk.Error.send_error(skt, 5)
                 continue
-            print(f"DEBUG: Getting old room of player: {player.room}")
             old_room = lurk.Room.get_room(player.room)
-            print(f"DEBUG: Old room: {old_room}")
             if changeroom.target_room not in old_room.connections:
                 print(f"{Fore.YELLOW}WARN: {player.name} attempted bad move, sending ERROR code 1!")
                 lurk.Error.send_error(skt, 1)
                 continue
             player = lurk.Character.get_character_with_name(sockets[skt])
             old_room = player.room
-            print(f"DEBUG: Old room: {old_room}")
-            print(f"DEBUG: Setting player room to {changeroom.target_room}")
             player.room = changeroom.target_room
-            print(f"DEBUG: player.room = {player.room}")
-            print("DEBUG: Updating character in database...")
             lurk.Character.update_character(player)
             new_room = lurk.Room.get_room(player.room)
             # Send new ROOM to player
             lurk.Room.send_room(skt, new_room)
             # Send updated CHARACTER to player
-            lurk.Character.send_character(skt, player)
+            #lurk.Character.send_character(skt, player)
             # Send all characters in new room to player
             # And send updated character to all players in new room that player entered new room
             characters = lurk.Character.get_characters_with_room(player.room)
