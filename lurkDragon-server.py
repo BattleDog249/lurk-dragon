677ddd27165c90e15ca948bd52f9b67de3a9c640
Logan Gray <lgray@proton.me>
2023-04-17 14:07:06 -0700

    Debugging

--- lurkDragon-server.py
+++ lurkDragon-server.py
@@ -248,7 +248,8 @@ def handle_client(skt):
                 continue
             print(f"{Fore.WHITE}DEBUG: Player {player.name} looting {loot.target_name}")
             target_name = loot.target_name
-            target = lurk.Character.get_character_with_name("Rattlesnake")
+            target_name = str(target_name, "utf-8").strip()
+            target = lurk.Character.get_character_with_name(target_name)
             print(f"{Fore.WHITE}DEBUG: Target: {target}")
             if target is None or target.room != player.room:
                 print(f"{Fore.YELLOW}WARN: Cannot loot nonexistent target, sending ERROR code 6!")
