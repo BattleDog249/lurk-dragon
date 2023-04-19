24e91d92de2ba495c40e56cb4fc2aec805bbf926
BattleDog249 <BattleDog249@proton.me>
2023-04-19 10:45:20 -0700

    Change to PyQt6

--- lurkDragon-client.py
+++ lurkDragon-client.py
@@ -1,52 +1,40 @@
 #!/usr/bin/env python3
 
-import curses
 import lurk
 import socket
+import sys
+from PyQt6.QtWidgets import (
+    QApplication,
+    QPushButton,
+    QMainWindow,
+    QVBoxLayout,
+    QWidget,
+    QLineEdit
+)
 
 from colorama import Fore
 
+class Window(QMainWindow):
+    pass
+
 # Variables defining host address & port of server to connect to
-HOST = 'localhost'
-PORT = 5010
+#HOST = 'localhost'
+#PORT = 5010
 
-def main(stdscr):
-    # Set up the screen
-    stdscr.clear()
-    stdscr.addstr(0, 0, "Press Ctrl+C to exit")
+# Connect to the server
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#s.connect((HOST, PORT))
 
-    # Create a window for user input
-    input_window = curses.newwin(3, curses.COLS, curses.LINES-3, 0)
-    input_window.addstr(0, 0, "Send a message: ")
-    input_window.refresh()
+app = QApplication([])
+window = QWidget()
+window.setWindowTitle("LurkDragon")
 
-    # Create a window for displaying messages from the server
-    display_window = curses.newwin(curses.LINES-3, curses.COLS, 0, 0)
-    display_window.scrollok(True)
-    display_window.refresh()
-    
-    # Connect to the server
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
+layout = QVBoxLayout()
+layout.addWidget(QPushButton("Connect to Server"))
+layout.ipTextBox = QLineEdit()
+layout.addWidget(QPushButton("Center"))
+layout.addWidget(QPushButton("Bottom"))
+window.setLayout(layout)
 
-    # Loop for user input
-    while True:
-        # Get user input
-        input_window.clear()
-        input_window.addstr(1, 0, "> ")
-        input_window.refresh()
-        message = input_window.getstr().decode()
-
-        # Send message to server
-        s.sendall(message.encode())
-
-        # Receive response from server
-        response = s.recv(1024).decode()
-
-        # Display response in the display window
-        display_window.addstr(response)
-        display_window.refresh()
-
-if __name__ == '__main__':
-    # Set up curses and run the main function
-    curses.wrapper(main)
+window.show()
+sys.exit(app.exec())
